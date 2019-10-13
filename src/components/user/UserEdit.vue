8c0dce82e55d8e399caf8cbb67a2b2d40e5cb6eb
saefudin <udin@gmail.com>
2019-10-13 19:55:51 +0700

    add query

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,3 +1,8 @@
 <template>
+  <div>
     <h3>Edit the User</h3>
+    <!-- value query abc.com/user/edit?locale=en&q=100-->
+    <p>Locale: {{ $route.query.locale}}</p>
+    <p>Analytics: {{ $route.query.q}}</p>
+  </div>
 </template>
